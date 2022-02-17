e3db7b781d690de75d6d7bb248db5d386e6033b9
Miikkael Peltokorpi <miikkael.peltokorpi@rakettitukku.fi>
2022-02-17 12:11:00 +0200

    added bubbleSort exercise

---
+++ BubbleSort/bubblesort.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+original_numbers=()
+new_numbers=()
+
+print_original_numbers()
+{
+    original_numbers+=($number)
+    echo "${original_numbers[@]}"
+    
+    indexes=${!original_numbers[@]}
+    len=${#original_numbers[@]}
+    echo "len $len"
+    
+    
+    
+    if [[ len -ge 0 ]] ; then
+    
+        if [[ original_numbers[-2] -gt original_numbers[-1] ]]; then
+          last=${original_numbers[-1]}
+          secondlast=${original_numbers[-2]}
+          new_numbers+=($last)
+          echo "${new_numbers[@]}"
+          echo 'swap'
+          
+          
+        else
+          new_numbers+=($secondlast)
+          echo "${new_numbers[@]}"
+          echo 'noswap'
+          
+        fi
+    fi
+
+}
+
+
+while :
+    read -p "Insert number" number    
+do  
+    
+    re='^[0-9]+$'
+    if [[ $number =~ $re ]]; then 
+        i=$(( $i + 1 ))
+        print_original_numbers "$number" "$i"
+    else
+        echo "Error: Not a number!"
+    fi 
+done
---
+++ BubbleSort/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+testi=("value" "value2")
+
+
+for str in ${testi[@]}; do
+  echo $str
+done
